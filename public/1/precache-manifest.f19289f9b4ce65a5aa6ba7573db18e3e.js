29b8db8524e5101700bea31e8212263a28ea21b5
Daniel <yfeijoo95@gmail.com>
2020-07-03 13:21:03 +0200

    first commit

---
+++ public/1/precache-manifest.f19289f9b4ce65a5aa6ba7573db18e3e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91564273283302ad561f6c3ecd62c900",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5693169424b741255ea6",
+    "url": "/static/css/2.7a75a24e.chunk.css"
+  },
+  {
+    "revision": "a3c64840d4ed064e544c",
+    "url": "/static/css/main.c85a7d68.chunk.css"
+  },
+  {
+    "revision": "5693169424b741255ea6",
+    "url": "/static/js/2.4fe885b6.chunk.js"
+  },
+  {
+    "revision": "a3c64840d4ed064e544c",
+    "url": "/static/js/main.8f0ba396.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a94f70d047866186a5624c41eff46b6",
+    "url": "/static/media/ArcadeClassic.5a94f70d.otf"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "fc21cea151f5aa0f837e6c2dd30306fb",
+    "url": "/static/media/linkedin.fc21cea1.png"
+  },
+  {
+    "revision": "e4d15341d738fd7fb486171ebc27973a",
+    "url": "/static/media/logo-big-dark.e4d15341.svg"
+  },
+  {
+    "revision": "21b2f5cd7f7aaf7eae64c0cbc53bf690",
+    "url": "/static/media/logoa.21b2f5cd.svg"
+  }
+]);
